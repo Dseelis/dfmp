12b738c8dd3f2d53290f87c25def76a527320752
Dseelis <113524235+Dseelis@users.noreply.github.com>
2026-02-19 20:20:06 +0200

    Add files via upload

---
+++ media/lua/shared/TCGMusicDefenitionsTCVinylplayer.lua
@@ -0,0 +1,9 @@
+require "TCMusicDefenitions"
+
+	GlobalMusic["VinylBluesForMrT"] = "tsarcraft_music_01_63"
+	GlobalMusic["VinylBudMeetsBob"] = "tsarcraft_music_01_63"
+	GlobalMusic["VinylCrawlingLinkinPark"] = "tsarcraft_music_01_63"
+	GlobalMusic["VinylIntheEndLinkinPark"] = "tsarcraft_music_01_63"
+	GlobalMusic["VinylMyDecemberLinkinPark"] = "tsarcraft_music_01_63"
+	GlobalMusic["VinylPointsofAuthorityLinkinPark"] = "tsarcraft_music_01_63"
+
